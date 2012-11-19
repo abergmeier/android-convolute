53f78e676bd04dd716636073650eb45000c5d930
Andreas Bergmeier <abergmeier@stud.hs-bremen.de>
2012-11-19 12:04:45 +0100

    Add noexcept information

--- test1/jni/fib.cpp
+++ test1/jni/fib.cpp
@@ -27,7 +27,7 @@ namespace {
 	typedef std::vector<color_type> color_buffer_type;
 
 	template <short SHIFT>
-	constexpr color_type color( rgb_type color ) {
+	constexpr color_type color( rgb_type color ) noexcept {
 		return (color >> SHIFT) & 0xFF;
 	}
 
@@ -37,34 +37,34 @@ namespace {
 		static const short BLUE  =  0;
 	};
 
-	constexpr color_type red  (rgb_type rgb) { return color<SHIFT::RED  >(rgb) & 0xFF; }
-	constexpr color_type green(rgb_type rgb) { return color<SHIFT::GREEN>(rgb) & 0xFF; }
-	constexpr color_type blue (rgb_type rgb) { return color<SHIFT::BLUE >(rgb) & 0xFF; }
+	constexpr color_type red  (rgb_type rgb) noexcept { return color<SHIFT::RED  >(rgb) & 0xFF; }
+	constexpr color_type green(rgb_type rgb) noexcept { return color<SHIFT::GREEN>(rgb) & 0xFF; }
+	constexpr color_type blue (rgb_type rgb) noexcept { return color<SHIFT::BLUE >(rgb) & 0xFF; }
 
 	template <short SHIFT>
-	constexpr void merge_value(rgb_type& rgb, color_type color) {
+	constexpr void merge_value(rgb_type& rgb, color_type color) noexcept {
 		rgb |= (color & 0xFF) << SHIFT;
 	}
 
 	template <short SHIFT>
-	void mergeFunc( buffer_type& collection, color_buffer_type::const_iterator it ) {
+	void mergeFunc( buffer_type& collection, color_buffer_type::const_iterator it ) noexcept {
 		for( auto& value : collection ){
 			merge_value<SHIFT>(value, *it);
 			++it;
 		}
 	}
 
-	constexpr size_t offset(size_t x, size_t y, size_t width) {
+	constexpr size_t offset(size_t x, size_t y, size_t width) noexcept {
 		return y * width + x;
 	}
 
 	template <short SHIFT>
-	color_type channel_pixel( const buffer_type& input, size_t x, size_t y, size_t width ) {
+	color_type channel_pixel( const buffer_type& input, size_t x, size_t y, size_t width ) noexcept {
 		return color<SHIFT>( input[offset(x, y, width)] );
 	}
 
 	template <short SHIFT>
-	std::vector<color_type> colorFunc( const buffer_type& input, size_t width, size_t height, int const * kernel ) {
+	std::vector<color_type> colorFunc( const buffer_type& input, size_t width, size_t height, int const * kernel ) noexcept {
 		std::vector<color_type> output( width * height );
 
 		for( size_t y = 0; y < height; ++y ) {
@@ -123,14 +123,14 @@ namespace {
 	typedef std::tuple<process_function, merge_function> function_tuple;
 
 	template <short SHIFT>
-	function_tuple make_tuple( buffer_type& buffer, size_t width, size_t height, int const * kernel ) {
+	function_tuple make_tuple( buffer_type& buffer, size_t width, size_t height, int const * kernel ) noexcept {
 		process_function proc  = std::bind( &colorFunc<SHIFT>, std::ref(buffer), width, height, kernel );
 
 		merge_function merge = std::bind( &mergeFunc<SHIFT>, std::ref(buffer), std::placeholders::_1 );
 		return function_tuple( proc, merge );
 	}
 
-	void proc( size_t width, size_t height, int const* kernel ) {
+	void proc( size_t width, size_t height, int const* kernel ) noexcept {
 		// Make sure we have enough space
 		static buffer_type BUFFER;
 		BUFFER.reserve( width * height );
