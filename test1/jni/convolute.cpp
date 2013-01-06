74397f7c605ab58a68c96617208a9e0e7cb4c126
Andreas Bergmeier <abergmeier@stud.hs-bremen.de>
2013-01-06 18:59:09 +0100

    Rework C++ implementation.

--- test1/jni/convolute.cpp
+++ test1/jni/convolute.cpp
@@ -20,51 +20,28 @@ namespace std {
 }
 
 namespace {
-	typedef std::uint32_t           rgb_type;
-	typedef std::vector<rgb_type>   buffer_type;
 	typedef std::uint8_t            color_type;
-	typedef std::vector<color_type> color_buffer_type;
-
-	template <short SHIFT>
-	constexpr color_type color( rgb_type color ) noexcept {
-		return (color >> SHIFT) & 0xFF;
-	}
+	typedef std::vector<color_type> buffer_type;
 
 	struct SHIFT {
-		static const short RED   = 16;
-		static const short GREEN =  8;
-		static const short BLUE  =  0;
+		static const short RED   = 0;
+		static const short GREEN = 1;
+		static const short BLUE  = 2;
 	};
 
-	constexpr color_type red  (rgb_type rgb) noexcept { return color<SHIFT::RED  >(rgb) & 0xFF; }
-	constexpr color_type green(rgb_type rgb) noexcept { return color<SHIFT::GREEN>(rgb) & 0xFF; }
-	constexpr color_type blue (rgb_type rgb) noexcept { return color<SHIFT::BLUE >(rgb) & 0xFF; }
-
-	template <short SHIFT>
-	constexpr void merge_value(rgb_type& rgb, color_type color) noexcept {
-		rgb |= (color & 0xFF) << SHIFT;
+	constexpr std::size_t offset( std::size_t x, std::size_t y, std::size_t width, std::uint8_t bytesPerPixel ) noexcept {
+		return (y * width + x) * bytesPerPixel;
 	}
 
 	template <short SHIFT>
-	void mergeFunc( buffer_type& collection, color_buffer_type::const_iterator it ) noexcept {
-		for( auto& value : collection ){
-			merge_value<SHIFT>(value, *it);
-			++it;
-		}
-	}
-
-	constexpr size_t offset(size_t x, size_t y, size_t width) noexcept {
-		return y * width + x;
+	color_type channel_pixel( const buffer_type& input,
+	                          std::size_t x, std::size_t y,
+	                          std::size_t width, std::uint8_t bytesPerPixel ) noexcept {
+		return input[offset(x, y, width, bytesPerPixel) + SHIFT];
 	}
 
 	template <short SHIFT>
-	color_type channel_pixel( const buffer_type& input, size_t x, size_t y, size_t width ) noexcept {
-		return color<SHIFT>( input[offset(x, y, width)] );
-	}
-
-	template <short SHIFT>
-	std::vector<color_type> colorFunc( const buffer_type& input, size_t width, size_t height, int const * kernel ) noexcept {
-		std::vector<color_type> output( width * height );
+	void colorFunc( const buffer_type& input, size_t width, size_t height, std::uint8_t bytesPerPixel, int const * kernel, std::uint8_t* output ) noexcept {
 
 		for( size_t y = 0; y < height; ++y ) {
 			for( size_t x = 0; x < width; ++x ) {
@@ -76,7 +53,7 @@ namespace {
 						const auto kern = kernel[(2 + dx) + (2 + dy) * 5];
 						// Save old value to enable under/overflow detection
 						auto old = value;
-						value += channel_pixel<SHIFT>( input, x + dx, y + dy, width ) * kern;
+						value += channel_pixel<SHIFT>( input, x + dx, y + dy, width, bytesPerPixel ) * kern;
 
 						// Handle underflow
 						if( kern < 0 && value > old )
@@ -88,7 +65,7 @@ namespace {
 					}
 				}
 
-				output[offset(x, y, width)] = value;
+				output[offset(x, y, width, bytesPerPixel) + SHIFT] = value;
 			}
 		}
 	/*
@@ -113,76 +90,12 @@ namespace {
 				}
 			}
 			*/
-
-		return output;
 	}
 
-	typedef std::function<color_buffer_type()> process_function;
-	typedef std::function<void(color_buffer_type::const_iterator)>    merge_function;
-	typedef std::tuple<process_function, merge_function> function_tuple;
-
-	template <short SHIFT>
-	function_tuple make_tuple( buffer_type& buffer, size_t width, size_t height, int const * kernel ) noexcept {
-		process_function proc  = std::bind( &colorFunc<SHIFT>, std::ref(buffer), width, height, kernel );
-
-		merge_function merge = std::bind( &mergeFunc<SHIFT>, std::ref(buffer), std::placeholders::_1 );
-		return function_tuple( proc, merge );
-	}
-	void exec( const std::array<function_tuple, 3> function_tuples,
-		           size_t width,
-		           size_t height ) __attribute__ ((pure));
-	// Use pure function to help optimizer
-	void exec( const std::array<function_tuple, 3> function_tuples,
-	           size_t width,
-	           size_t height ) {
-		buffer_type output( width * height, 0xFF000000 );
-
-		{
-			// Try to move all resulting output to temporary
-			// array so we remove data dependency and
-			// enable vectorizing
-			std::array<color_buffer_type, 3> outputs;
-
-			{
-				auto outputIt = outputs.begin();
-
-				// Data should have no dependency
-				for( auto tuple : function_tuples ) {
-					// Apply serialized so no locking necessary
-
-					process_function function;
-					std::tie( function, std::ignore ) = tuple;
-
-					// Get processed channel
-					*outputIt = function();
-				}
-			}
-
-			{
-				auto outputIt = outputs.begin();
-
-				for( auto tuple : function_tuples ) {
-					merge_function merger;
-					std::tie( std::ignore, merger ) = tuple;
-
-					merger( outputIt->begin() );
-				}
-				// After last merge result should be sufficient
-			}
-		}
-	}
-
-	const buffer_type& convolute( buffer_type&  buffer, size_t width, size_t height, int32_t const* kernel ) noexcept {
-
-		auto function_tuples = [&]() -> std::array<function_tuple, 3> {
-			std::array<function_tuple, 3> temp{{
-				make_tuple<SHIFT::RED  >(buffer, width, height, kernel),
-				make_tuple<SHIFT::GREEN>(buffer, width, height, kernel),
-				make_tuple<SHIFT::BLUE >(buffer, width, height, kernel) }};
-			return temp;
-		}();
-
-		exec( function_tuples, width, height );
+	void convolute( const buffer_type&  buffer, std::size_t width, std::size_t height, std::uint8_t bytesPerPixel, int32_t const* kernel, std::uint8_t* output ) noexcept {
+		colorFunc<SHIFT::RED  >( buffer, width, height, bytesPerPixel, kernel, output );
+		colorFunc<SHIFT::GREEN>( buffer, width, height, bytesPerPixel, kernel, output );
+		colorFunc<SHIFT::BLUE >( buffer, width, height, bytesPerPixel, kernel, output );
 	}
 
 	template <typename T>
@@ -191,19 +104,20 @@ namespace {
 	}
 
 	template <typename T>
-	inline T clamp8( T value ) {
+	inline T clamp_u8( T value ) {
 		return clamp( value, 0, 255 );
 	}
 
-	inline void convertYUVtoRGB( std::uint8_t y, std::uint8_t u, std::uint8_t v, buffer_type& rgb, std::size_t index ) {
-		const auto rs = y + (unsigned short)(1.402f * v);
-		const auto gs = y - (unsigned short)(0.344f * u + 0.714f * v);
-		const auto bs = y + (unsigned short)(1.772f * u);
+	inline void convertYUVtoRGB( std::uint8_t y, std::int8_t u, std::int8_t v, std::uint8_t bytesPerPixel, buffer_type& rgb, std::size_t index ) {
+		const auto r = y + static_cast<std::int16_t>(1.402f * v);
+		const auto g = y - static_cast<std::int16_t>(0.344f * u + 0.714f * v);
+		const auto b = y + static_cast<std::int16_t>(1.772f * u);
 		// Normalize values
 
-		rgb[ index + 0 ] = clamp8( rs );
-		rgb[ index + 1 ] = clamp8( gs );
-		rgb[ index + 2 ] = clamp8( bs );
+		index = index * bytesPerPixel;
+		rgb[ index + 0 ] = clamp_u8( r );
+		rgb[ index + 1 ] = clamp_u8( g );
+		rgb[ index + 2 ] = clamp_u8( b );
 	}
 
 	jmethodID getMethod( JNIEnv * pEnv, jobject progress, const char* name, const char* signature ) {
@@ -244,38 +158,19 @@ namespace {
 
 			auto u = data[k + offset + 0];
 			auto v = data[k + offset + 1];
-			u = u-128;
-			v = v-128;
+			u = u - 128;
+			v = v - 128;
 
-			convertYUVtoRGB( y1, u, v, rgb, i + 0         );
-			convertYUVtoRGB( y2, u, v, rgb, i + 1         );
-			convertYUVtoRGB( y3, u, v, rgb, i + width + 0 );
-			convertYUVtoRGB( y4, u, v, rgb, i + width + 1 );
+			convertYUVtoRGB( y1, u, v, bytesPerPixel, rgb, i + 0         );
+			convertYUVtoRGB( y2, u, v, bytesPerPixel, rgb, i + 1         );
+			convertYUVtoRGB( y3, u, v, bytesPerPixel, rgb, i + width + 0 );
+			convertYUVtoRGB( y4, u, v, bytesPerPixel, rgb, i + width + 1 );
 
-			if (i != 0 && (i + 2) % width == 0 ) {
+			if(i != 0 && (i + 2) % width == 0 ) {
 				i += width;
 				increaseProgress( width * 2 );
 			}
 		}
-	}
-
-/*
-	// Compile time processing
-	template <unsigned int N>
-	inline unsigned int fib_4() {
-		if( N == 0 )
-			return 0;
-		if( N == 1 )
-			return 1;
-		return fib_4<N-1>() + fib_4<N-2>();
-	}
-*/
-	std::clock_t now() {
-		return std::clock() / (CLOCKS_PER_SEC / 1000);
-	}
-
-	long duration( std::clock_t t0, std::clock_t t1 ) {
-		return t1 - t0;
 	}
 
 	const char* LOG_TAG = "CONVOLUTE";
@@ -316,6 +211,7 @@ Java_de_hsbremen_android_convolution_nio_Processor_nativeProcess( JNIEnv * pEnv,
 	}
 
 	std::copy( BUFFER.begin(), BUFFER.end(), output );
+	//convolute( BUFFER, width, height, kernel, bytePerPixel, output );
 }
 
 /*
@@ -329,7 +225,7 @@ void JNICALL Java_de_hsbremen_android_convolution_nio_Processor_Process(JNIEnv *
 
 	convertYUV420_NV21toRGB888( frameBuffer, width, height, BUFFER );
 
-	convolute( BUFFER, width, height, kernel );
+	convolute( BUFFER, width, height, kernel, bytePerPixel );
 
 	// Copy data to GL_TEXTURE_EXTERNAL_OES storage
 	glTexImage2D( GL_TEXTURE_EXTERNAL_OES,
