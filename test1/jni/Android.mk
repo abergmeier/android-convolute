b855ba42d72c74f2b4b66dae3f900cdb4fda03f3
Andreas Bergmeier <abergmeier@stud.hs-bremen.de>
2013-01-06 20:00:12 +0100

    Remove outcommented lines from Android.mk

--- test1/jni/Android.mk
+++ test1/jni/Android.mk
@@ -8,10 +8,5 @@ LOCAL_LDLIBS := -llog
 LOCAL_CFLAGS += -ffast-math -fno-unsafe-math-optimizations -fomit-frame-pointer
 LOCAL_CPPFLAGS += -std=gnu++11 -O3
 LOCAL_LDFLAGS += -no-canonical-prefixes
-#LOCAL_CPP_FEATURES += exceptions rtti
-#LOCAL_STATIC_LIBRARIES += cpufeatures
 
 include $(BUILD_SHARED_LIBRARY)
-
-#$(call import-module,android/cpufeatures)
-
